ff630a1e000feafe64bf1fea9aba10ad3c963a6b
Michael Schout <schoutm@gmail.com>
2022-09-17 14:46:13 -0500

    Flesh out the rest of the server handler logic

--- src/main/java/io/github/mschout/srsd/postfix/SRSServerHandler.java
+++ src/main/java/io/github/mschout/srsd/postfix/SRSServerHandler.java
@@ -1,10 +1,12 @@
 package io.github.mschout.srsd.postfix;
 
+import com.google.common.base.Strings;
 import io.github.mschout.email.srs.SRS;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import java.security.InvalidKeyException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
@@ -24,24 +26,60 @@ public class SRSServerHandler extends ChannelInboundHandlerAdapter {
   public void channelRead(@NotNull ChannelHandlerContext ctx, @NotNull Object msg) throws Exception {
     String request = (String) msg;
 
+    String response = handleRequest(msg);
+
+    ChannelFuture future = ctx.writeAndFlush(response);
+
+    future.addListener(ChannelFutureListener.CLOSE);
+  }
+
+  private String handleRequest(Object msg) {
+    String request = (String) msg;
+
     if (request.toLowerCase().startsWith(CMD_FORWARD_PREFIX)) {
-      String address = request.substring(CMD_FORWARD_PREFIX.length());
-      String target = srs.forward(address, localAlias);
-
-      if (!address.equalsIgnoreCase(target)) log.info("Rewriting SRS Address {} to {}", address, target);
-
-      ChannelFuture future = ctx.writeAndFlush(target);
-      future.addListener(ChannelFutureListener.CLOSE);
+      return forwardAddress(request.substring(CMD_FORWARD_PREFIX.length()));
     } else if (request.toLowerCase().startsWith(CMD_REVERSE_PREFIX)) {
-      String address = request.substring(CMD_REVERSE_PREFIX.length());
-      String target = srs.reverse(address);
-
-      if (!address.equalsIgnoreCase(target)) log.info("Reversed SRS address {} to {}", address, target);
-
-      ChannelFuture future = ctx.writeAndFlush(target);
-      future.addListener(ChannelFutureListener.CLOSE);
+      return reverseAddress(request.substring(CMD_REVERSE_PREFIX.length()));
     } else {
       throw new IllegalArgumentException("Unrecognized SRS request: {}" + request);
     }
+  }
+
+  private String forwardAddress(@NotNull String address) {
+    try {
+      if (!address.contains("@")) {
+        return "NOTFOUND address does not contain domain";
+      }
+
+      String forward = srs.forward(address, localAlias);
+      if (Strings.isNullOrEmpty(forward)) {
+        log.error("SRS Forwarding for address {} failed, got null or empty address", forward);
+        return "PERM srs forwarding failed";
+      }
+
+      log.info("rewrite {} -> {}", address, forward);
+      return "OK " + forward;
+    } catch (InvalidKeyException e) {
+      log.warn("Invalid SRS Key exception on address {}: {}", address, e.getMessage());
+      return "NOTFOUND " + e.getMessage();
+    }
+  }
+
+  private String reverseAddress(@NotNull String address) {
+    if (!srs.isSRS(address)) return "NOTFOUND address is not SRS encoded";
+
+    if (!address.contains("@")) return "NOTFOUND address does not contain a domain";
+
+    if (!address.toLowerCase().endsWith("@" + localAlias.toLowerCase())) return "NOTFOUND external domains are ignored";
+
+    String reverse = srs.reverse(address);
+
+    if (Strings.isNullOrEmpty(reverse)) {
+      log.error("Failed to reverse address {}: received empty or null address", address);
+      return "NOTFOUND invalid srs email";
+    }
+
+    log.info("rewrite {} -> {}", address, reverse);
+    return "OK " + reverse;
   }
 }
